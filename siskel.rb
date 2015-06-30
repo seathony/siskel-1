be4979fd1081dc3d59773f08c80a891d21729954
Sean Sellek <me@seansellek.com>
2015-06-30 14:50:51 -0400

    finished Hackathon

--- siskel.rb
+++ siskel.rb
@@ -1,16 +1,36 @@
 require 'httparty'
 
 class Siskel
-
-  def initialize(title)
-    movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}")
-    @title = movie["Title"]
+  attr_accessor :title, :rating, :year, :plot, :options, :tomato_score
+  def initialize(title, options = {})
+    @options = options
+    url = 'http://www.omdbapi.com/?' + options_builder(title, @options)
+    movie = HTTParty.get(url)
+    @tomato_score = movie['tomatoMeter'].to_i
+    @title = movie['Title'] || 'Movie not found!'
+    @rating = movie['Rated']
+    @year = movie['Year']
+    @plot = movie['Plot']
   end
-end 
 
+  def options_builder(title, options)
+    parameters = []
+    parameters.push "t=#{title}"
+    parameters.push 'tomatoes=true'
+    parameters.push "y=#{options[:year]}" if options[:year]
+    parameters.push "plot=#{options[:plot]}" if options[:plot]
+    parameters.join('&')
+  end
 
-
-
-
-
-
+  def concensus
+    if @tomato_score.between?(76, 100)
+      'Two Thumbs Up'
+    elsif @tomato_score.between?(51, 75)
+      'Thumbs Up'
+    elsif @tomato_score.between?(26, 50)
+      'Thumbs Down'
+    else
+      'Two Thumbs Down'
+    end
+  end
+end
